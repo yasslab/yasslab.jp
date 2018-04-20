321f178bb4efc2532b38d9a6fa9fbd5487375d8b
aokabin <info.aokabin@gmail.com>
2018-04-20 15:22:07 +0900

    Create github_statistics.js

---
+++ js/github_statistics.js
@@ -0,0 +1,47 @@
+var retry = 0;
+const MAX_RETRY = 3;
+
+const modifyDom = function (statistics) {
+  // write to modify dom code
+  console.log(statistics);
+}
+
+function calcGithubStatistics(result) {
+  var github_json_data = JSON.parse(result.target.responseText);
+  var repo_count = github_json_data.length;
+  var github_statistics = {
+    repositories: repo_count,
+    stars: 0
+  };
+  for (var i = 0; i < repo_count; i++) {
+    github_statistics.stars += github_json_data[i].stargazers_count;
+  }
+
+  return github_statistics;
+}
+
+function githubSuccessCallback(result) {
+  if (result.target.status != 200) {
+    retry++;
+    if (MAX_RETRY < retry) {
+      console.log(retry);
+      getGithubData();
+    }
+    return
+  }
+
+  var github_statistics = calcGithubStatistics(result);
+  modifyDom(github_statistics);
+}
+
+function getGithubData() {
+  const request = new XMLHttpRequest();
+  const uri = 'https://api.github.com/orgs/yasslab/repos';
+
+  request.open('GET', uri);
+  request.setRequestHeader('Accept', 'application/vnd.github.preview')
+  request.addEventListener('load', githubSuccessCallback, false);
+  request.send();
+}
+
+getGithubData();
