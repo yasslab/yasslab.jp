3448781a74bfd642a68c08624708555214d10c55
aokabin <info.aokabin@gmail.com>
2018-04-27 14:18:28 +0900

    Create github_statistics test and update code simply

--- js/github_statistics.js
+++ js/github_statistics.js
@@ -1,40 +1,66 @@
-var retry = 0;
+var retry_count = 0;
 const MAX_RETRY = 3;
 
-var statistics;
+var error_stats = {
+  repositories: "🙏",
+  stars: "🙇"
+};
 
-const modifyDom = function (stat) {
-  // write to modify dom code
-  console.log(stat);
+const modifyDom = function (stats) {
+  var repositories = document.getElementById("github__repositories");
+  var stars = document.getElementById("github__stars");
+
+  if (repositories && stars) {
+    repositories.textContent = stats.repositories;
+    stars.textContent = stats.stars;
+  }
 }
 
 function calcGithubStatistics(result) {
   var github_json_data = JSON.parse(result.target.responseText);
   var repo_count = github_json_data.length;
+
   var github_statistics = {
     repositories: repo_count,
     stars: 0
   };
+
   for (var i = 0; i < repo_count; i++) {
     github_statistics.stars += github_json_data[i].stargazers_count;
   }
 
-  statistics = github_statistics;
   return github_statistics;
 }
 
 function githubSuccessCallback(result) {
+  var is_succeed = false;
   if (result.target.status != 200) {
-    retry++;
-    if (MAX_RETRY < retry) {
-      console.log(retry);
+    if (retry_count < MAX_RETRY) {
+      retry_count++;
       getGithubData();
+    } else {
+      modifyDom(error_stats);
     }
-    return
+  } else {
+    var github_statistics = calcGithubStatistics(result);
+    modifyDom(github_statistics);
+    is_succeed = true;
   }
 
-  var github_statistics = calcGithubStatistics(result);
-  modifyDom(github_statistics);
+  return is_succeed;
+}
+
+function githubErrorCallback(result) {
+  var is_retried = true;
+  if (retry_count < MAX_RETRY) {
+    retry_count++;
+    getGithubData();
+  } else {
+    modifyDom(error_stats);
+    is_retried = false;
+  }
+
+  return is_retried;
 }
 
 function getGithubData() {
@@ -44,8 +70,8 @@ function getGithubData() {
   request.open('GET', uri);
   request.setRequestHeader('Accept', 'application/vnd.github.preview')
   request.addEventListener('load', githubSuccessCallback, false);
+  request.addEventListener('error', githubErrorCallback, false);
   request.send();
 }
 
 getGithubData();
-console.log(statistics); // すぐには反映されない
--- js/test/github_statistics-spec.js
+++ js/test/github_statistics-spec.js
@@ -1,12 +1,13 @@
-describe('calcGitHubStatistics', function() {
+describe('github_statistics.js', function() {
   describe('10 repositories each have 100 stars', function () {
     var mock_http_result = {
       target: {
-        responseText: ""
+        responseText: "",
+        status: 200
       }
     };
 
-    before(function() {
+    before(function () {
       var prepare = [];
       for (var i = 0; i < 10; i++) {
         var repository_data = {
@@ -18,20 +19,117 @@ describe('calcGitHubStatistics', function() {
       mock_http_result.target.responseText = JSON.stringify(prepare);
     });
 
-    it('return 10 repositories', function() {
-      var result = calcGithubStatistics(mock_http_result);
-      if (result.repositories === 10) {
-      } else {
-        throw new Error('repository count is not correct');
-      }
+    describe('#calcGitHubStatistics', function() {
+      it('should return 10 repositories', function() {
+        var result = calcGithubStatistics(mock_http_result);
+        if (result.repositories === 10) {
+        } else {
+          throw new Error('repository count is not correct');
+        }
+      });
+
+      it('should return 1000 stars', function() {
+        var result = calcGithubStatistics(mock_http_result);
+        if (result.stars === 1000) {
+        } else {
+          throw new Error('total star count is not correct');
+        }
+      });
     });
 
-    it('return 1000 stars', function() {
-      var result = calcGithubStatistics(mock_http_result);
-      if (result.stars === 1000) {
-      } else {
-        throw new Error('total star count is not correct');
-      }
+    describe('#githubSuccessCallback', function() {
+      it('should return true when status is 200', function() {
+        var is_succeed = githubSuccessCallback(mock_http_result);
+        if (is_succeed == false) {
+          throw new Error("success call back should return true but return "+is_succeed);
+        }
+      });
+
+      it('should return false data when status is not 200', function() {
+        mock_http_result.target.status = 404;
+        var is_succeed = githubSuccessCallback(mock_http_result);
+        if (is_succeed == true) {
+          throw new Error("success call back should return false but return "+is_succeed);
+        }
+      });
+    });
+
+    describe('#githubErrorCallback', function() {
+      it('should retry when retry_count < MAX_RETRY', function() {
+        retry_count = 0;
+        var is_retried = githubErrorCallback(mock_http_result);
+        console.log("is retried = "+is_retried);
+        if (is_retried == false) {
+          throw new Error("error call back should return true but return "+is_retried);
+        }
+      });
+
+      it('should not retry when retry_count >= MAX_RETRY', function() {
+        retry_count = MAX_RETRY;
+        var is_retried = githubErrorCallback(mock_http_result);
+        if (is_retried == true) {
+          throw new Error("error call back should return false but return "+is_retried);
+        }
+      });
+    });
+
+    describe('#modifyDom', function() {
+      var stats;
+      beforeEach(function () {
+        var new_repositories_element = document.createElement("p");
+        new_repositories_element.setAttribute("id", "github__repositories");
+        new_repositories_element.setAttribute("style", "display:none;");
+        document.body.appendChild(new_repositories_element);
+
+        var new_stars_element = document.createElement("p");
+        new_stars_element.setAttribute("id", "github__stars");
+        new_stars_element.setAttribute("style", "display:none;");
+        document.body.appendChild(new_stars_element);
+
+        stats = calcGithubStatistics(mock_http_result);
+      });
+
+      afterEach(function () {
+        var repositories = document.getElementById("github__repositories");
+        var stars = document.getElementById("github__stars");
+        repositories.parentNode.removeChild(repositories);
+        stars.parentNode.removeChild(stars);
+      });
+
+      describe('when github return status 200', function() {
+        it('should show correct stars count as 1000', function() {
+          modifyDom(stats);
+          var stars = document.getElementById("github__stars").innerText;
+          if (stars !== "1000") {
+            throw new Error('total star count should be correct count but show '+stars);
+          }
+        });
+
+        it('should show correct repository count as 10', function() {
+          modifyDom(stats);
+          var repositories = document.getElementById("github__repositories").innerText;
+          if (repositories !== "10") {
+            throw new Error('total repository count should be correct count but show '+repositories);
+          }
+        });
+      });
+      describe('when github continue to return status without 200 over MAX_RETRY', function() {
+        it('should show error stars like 🙇 ', function() {
+          modifyDom(error_stats);
+          var stars = document.getElementById("github__stars").innerText;
+          if (stars !== "🙇") {
+            throw new Error('total star count should not be count');
+          }
+        });
+
+        it('should show error repositories like 🙏 ', function() {
+          modifyDom(error_stats);
+          var repositories = document.getElementById("github__repositories").innerText;
+          if (repositories !== "🙏") {
+            throw new Error('total repository count should not be count');
+          }
+        });
+      });
     });
   });
 });
