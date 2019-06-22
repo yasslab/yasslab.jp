6992d1920f3ba94ba1740f0ad3c0fab2409b73e1
Yohei Yasukawa <yohei@yasslab.jp>
2019-06-22 12:24:23 +0900

    Enable 'yuppy' to easier write down post by script

--- _posts/create-draft-from-template.sh
+++ _posts/create-draft-from-template.sh
@@ -11,11 +11,19 @@ else
     /bin/echo -n "Generated "
 
     TEMPLATE_FILENAME=`ruby -e 'puts Time.now.strftime("%Y-%m-%d") + "-#{ARGV[0]}.md"' $1`
+
+    # Write down here if your screen name
+    # is different from your Twitter name.
+    AUTHOR=`whoami`
+    case $AUTHOR in
+	"yuppy" ) AUTHOR="Yuppyhappytoyou" ;;
+    esac
+
     echo "---"                     >  $TEMPLATE_FILENAME
     echo "layout: post"            >> $TEMPLATE_FILENAME
     echo "title:  タイトル"        >> $TEMPLATE_FILENAME
     echo "thumbnail: bg-sky.jpg"   >> $TEMPLATE_FILENAME
-    echo "author: `whoami`"        >> $TEMPLATE_FILENAME
+    echo "author: ${AUTHOR}"       >> $TEMPLATE_FILENAME
     echo "categories: [blog]"      >> $TEMPLATE_FILENAME
     echo "tags: [登壇, Rails]"     >> $TEMPLATE_FILENAME
     echo "permalink: /ja/posts/$1" >> $TEMPLATE_FILENAME
