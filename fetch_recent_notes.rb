439867c54b8f7f92db05c459d4287e0ddcc7771f
Yohei Yasukawa <yohei@yasslab.jp>
2020-08-14 14:30:58 +0900

    Fetch articles from note.com

--- fetch_recent_notes.rb
+++ fetch_recent_notes.rb
@@ -7,17 +7,16 @@ require 'mechanize'
 # How many articles you want to output? (Default: 3)
 number_of_fetching_articles = (ARGV[0] || '3').to_i
 
-#YASSLAB_NOTE_RSS = 'https://note.com/yasslab/rss'
-YASSLAB_NOTE_RSS = 'note_yasslab.rss' # for debugging
+YASSLAB_NOTE_RSS = 'https://note.com/yasslab/rss'
+#YASSLAB_NOTE_RSS = 'note_yasslab.rss' # for debugging
 rss = RSS::Parser.parse(YASSLAB_NOTE_RSS, false)
 
 # Parse RSS and get yaml text
 rss.items.each_with_index do |item, index|
   break if index >= number_of_fetching_articles
-  title_emoji = Mechanize.new.get(item.link).title[0]
 
   puts <<YAML_TEXT
-- title: #{title_emoji + item.title}
+- title: #{item.title}
   date:  #{item.pubDate.strftime("%Y-%m-%d")}
   url:   #{item.link}
 
