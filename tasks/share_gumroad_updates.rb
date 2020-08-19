cbfb16e1fa5c07f0d26d9c2c02dab8dd33b4befe
Yohei Yasukawa <yohei@yasslab.jp>
2020-08-19 10:27:54 +0900

    Set timezone in global env

--- tasks/share_gumroad_updates.rb
+++ tasks/share_gumroad_updates.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
-ENV['TZ'] = 'Asia/Tokyo'
 unless Time.now.hour == 9 && Time.now.min >= 30
-  puts 'Current Time: ' + Time.now.strftime("%H:%M")
+  puts 'Current Time: ' + Time.now.strftime("%H:%M") + "(TZ=#{ENV['TZ']})"
   puts 'This task runs only 09:30..10:00. :sleeping:'
   return
 end
