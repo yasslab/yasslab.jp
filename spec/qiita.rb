ff9ce9b785205349e20d785430466d1642a165e1
Yohei Yasukawa <yohei@yasslab.jp>
2021-06-24 18:07:26 +0900

    Fix test suite for qiita plugin

--- spec/qiita.rb
+++ spec/qiita.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 require 'nokogiri'
 require 'jekyll'
 
-SUCESS_MOCK_PATH = 'spec/mock.html'.freeze
+SUCESS_MOCK_PATH = '_data/qiita_org_sample.html'.freeze
 FAILED_MOCK_PATH = 'spec/error_mock.html'.freeze
 
 RSpec.describe 'Qiita' do
   require './_plugins/qiita.rb'
-  it 'qiita-like-and-items' do
+  it 'stats should get items & likes with correct path' do
     mock = File.expand_path(SUCESS_MOCK_PATH)
 
     items = Liquid::Template.parse("{% qiita_stats items %}")
     expect(items.render).to be == '80'
     likes = Liquid::Template.parse("{% qiita_stats likes %}")
-    expect(likes.render).to be == '6710'
+    expect(likes.render).to be == '7000'
   end
 
-  it 'not possible to obtain' do
+  it 'stats should fail to access with wrong path' do
     mock = File.expand_path(FAILED_MOCK_PATH)
 
     items = Liquid::Template.parse("{% qiita_stats items %}")
