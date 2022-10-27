5eaacb6ab924187ff6ffe864134ec417dada4eff
Yohei Yasukawa <yohei@yasslab.jp>
2022-10-27 12:36:11 +0900

    Update Puma settings based on https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server

--- puma.rb
+++ puma.rb
@@ -1,16 +1,18 @@
 require 'yaml'
 
-# web: bundle exec puma -t 8:32 -w 3 -p $PORT
-# Example settings: https://github.com/puma/puma-heroku/blob/master/lib/puma/plugin/heroku.rb
-workers_count = Integer(ENV['WEB_CONCURRENCY'] || 1)
+# web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
+# Doc: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
+# Example: https://github.com/puma/puma-heroku/blob/master/lib/puma/plugin/heroku.rb
+workers_count = Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS']     || 5)
+
+workers workers_count
 threads threads_count, threads_count
+preload_app!
 
-if workers_count > 1
-  preload_app!
-  workers workers_count
-end
-
+# NOTE: DefaultRackup is removed from Puma 6
+# https://github.com/puma/puma/blob/master/6.0-Upgrade.md
+rackup      DefaultRackup if defined?(DefaultRackup)
 port        ENV['PORT']     || 4000
 environment ENV['RACK_ENV'] || 'development'
 
